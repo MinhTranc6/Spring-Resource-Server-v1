79a19cc5f6ed6582a2ac03ba3407ac15bafc988d
MinhTranc6 <minhtranc6@gmail.com>
2025-11-22 21:14:31 +0700

    Initial commit.

---
+++ src/main/java/minhtranc6/Spring_Resource_Server_v1/SpringResourceServerV1Application.java
@@ -0,0 +1,13 @@
+package minhtranc6.Spring_Resource_Server_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceServerV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceServerV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/minhtranc6/Spring_Resource_Server_v1/SpringResourceServerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package minhtranc6.Spring_Resource_Server_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceServerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
